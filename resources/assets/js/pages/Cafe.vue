8ca967d7199bb0f157d25aa70d814a14319029fe
chain24 <chainsy97@gmail.com>
2018-12-04 11:30:57 +0800

    还原删除的js目录

---
+++ resources/assets/js/pages/Cafe.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Cafe"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/pages/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app-layout">
+        <navigation></navigation>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import Navigation from '../components/global/Navigation.vue';
+    export default {
+        components: {
+            Navigation
+        },
+        created(){
+            this.$store.dispatch( 'loadCafes' );
+            this.$store.dispatch( 'loadUser' );
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
