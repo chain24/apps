c0231886523ff490f91067a7c38900c2a8f97e6e
chainw <chainsy97@gmail.com>
2018-12-13 17:01:55 +0800

    cafe模型添加关联关系

--- app/Models/Cafe.php
+++ app/Models/Cafe.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cafe extends Model
 {
-    //
+    //Cafe 模型类与 BrewMethod 模型类之间的多对多关联
+    public function brewMethods()
+    {
+        return $this->belongsToMany(BrewMethod::class,'cafes_brew_methods','cafe_id','brew_method_id');
+    }
 }
