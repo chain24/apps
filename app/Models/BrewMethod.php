6d1aa245cda7b0bcbaf05b0ffda6f03f4eebe703
chainw <chainsy97@gmail.com>
2018-12-13 16:58:18 +0800

    冲泡方法模型添加关联关系

--- app/Models/BrewMethod.php
+++ app/Models/BrewMethod.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class BrewMethod extends Model
 {
-    //
+    protected $table = 'brew_methods';
+    //定义cafe 与 brew_methods之间多对多的关系
+    public function cafes()
+    {
+        return $this->belongsToMany(Cafe::class, 'cafes_brew_methods','brew_method_id', 'cafe_id');
+    }
 }
