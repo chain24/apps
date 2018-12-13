4271a54349f46f85ce86b7807b4ae81c4c847fe0
chainw <chainsy97@gmail.com>
2018-12-13 17:58:22 +0800

    添加获取冲泡方法

---
+++ resources/assets/js/modules/brewMethods.js
@@ -0,0 +1,56 @@
+/*
+|-------------------------------------------------------------------------------
+| VUEX modules/brewMethod.js
+|-------------------------------------------------------------------------------
+| The Vuex data store for the brew methods
+*/
+
+/**
+ *  status = 0 -> 数据尚未加载
+ *  status = 1 -> 数据开始加载
+ *  status = 2 -> 数据加载成功
+ *  status = 3 -> 数据加载失败
+ */
+
+import BrewMethodAPI from '../api/brewMethod.js';
+export const brewMethods = {
+    state: {
+        brewMethods: [],
+        brewMethodsLoadStatus: 0,
+    },
+    actions: {
+        loadBrewMethods( { commit } ){
+            commit( 'setBrewMethodsLoadStatus', 1 );
+
+            BrewMethodAPI.getBrewMethods()
+                .then( function( response ){
+                    commit( 'setBrewMethods', response.data );
+                    commit( 'setBrewMethodsLoadStatus', 2 );
+                })
+                .catch( function(){
+                    commit( 'setBrewMethods', [] );
+                    commit( 'setBrewMethodsLoadStatus', 3 );
+                });
+        },
+    },
+    mutations: {
+        setBrewMethodsLoadStatus( state, status ){
+            state.brewMethodsLoadStatus = status;
+        },
+
+        setBrewMethods( state, brewMethod ){
+            state.brewMethod = brewMethod;
+        }
+    },
+    getters: {
+        getBrewMethods( state ){
+            return state.brewMethods;
+        },
+
+        getBrewMethodsLoadStatus( state ){
+            return state.brewMethodsLoadStatus;
+        },
+
+    }
+
+}
