0179b504b823b8e31e832152c1d08f09fb117e99
chainw <chainsy97@gmail.com>
2018-12-13 17:38:30 +0800

    添加api获取冲泡方法

--- routes/api.php
+++ routes/api.php
@@ -52,4 +52,14 @@ Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
      | Description:    Adds a new cafe to the application
     */
     Route::post('/cafes', 'API\CafesController@postNewCafe');
+    /*
+     |-------------------------------------------------------------------------------
+     | Get All Methods
+     |-------------------------------------------------------------------------------
+     | URL:            /api/v1/brew-methods
+     | Controller:     API\BrewMethodsController@getBrewMethods
+     | Method:         GET
+     | Description:    Gets all of the brew-methods in the application
+    */
+    Route::get('/brew-methods', 'API\BrewMethodsController@getBrewMethods');
 });
